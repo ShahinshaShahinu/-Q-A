25a023320f7b58743d2e0c7fd0d9e77f3fe35be8
ShahinshaShahinu <muhammedshahinsha2442@gmail.com>
2024-03-30 00:19:30 +0530

    MOngo-Db file created

---
+++ Mongo-DB/sampl.js
@@ -0,0 +1,2 @@
+db.employeeSalary.aggregate([ { $group: { _id: '$department', MaxSalary: { $max: '$salary' }, MinSalary: { $min: '$salary' } } }, 
+{ $sort: { MaxSalary: -1 } }, { $facet: { MaxSalary: [{ $limit: 1 }], MinSalary: [{ $sort: { MinSalary: 1 } }, { $limit: 1 }] } }] )
